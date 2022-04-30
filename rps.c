73cbd145dbd959f27874cba0900be5c60933c377
jonathanrivlin <89954493+jonathanrivlin@users.noreply.github.com>
2022-04-30 17:46:11 +0300

    do while to play again + strcmp instead strlen

--- rps.c
+++ rps.c
@@ -1,42 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
 
 int main(){
-	const char winCases[3][2][10] = {
-		{"rock", "scissors"},
-		{"scissors", "paper"},
-		{"paper", "rock"}
-	};
+    char PlayerInterestedToPlay;
+    do{
+        system("clear");
+        PlayerInterestedToPlay = ' ';
+        const char winCases[3][2][10] = {
+            {"rock", "scissors"},
+            {"scissors", "paper"},
+            {"paper", "rock"}
+        };
 
-	srand(time(NULL));
-	const int randomNumber = rand() % 3;
-	const char options[3][10] = {"scissors", "paper", "rock"};
-	const char *pcChoice = options[randomNumber];
+        srand(time(NULL));
+        const int randomNumber = rand() % 3;
+        const char options[3][10] = {"scissors", "paper", "rock"};
+        const char *pcChoice = options[randomNumber];
 
-	char userChoice[10];
-	printf("Enter your selection(rock/paper/scissors): \n");
-	scanf("%s", userChoice);
-	printf("The computer choice is %s\n", pcChoice);
+        char userChoice[10];
+        printf("\nEnter your selection(rock/paper/scissors): \n");
+        scanf("%s", userChoice);
+        printf("\nThe computer choice is %s\n\n", pcChoice);
 
-	char *gameCase[2] = {userChoice, pcChoice};
+        char *gameCase[2] = {userChoice, pcChoice};
 
-	int i;
-	int checkForWin = 0;
-	for (i = 0; i < 3; ++i){
-	    if (strlen(winCases[i][0]) == strlen(gameCase[0]) && strlen(winCases[i][1]) == strlen(gameCase[1])){
-				checkForWin = 1;
-			}
-	}
+        int i;
+        int checkForWin = 0;
+        for (i = 0; i < 3; ++i){
+            if (strcmp(winCases[i][0], gameCase[0]) == 0 && strcmp(winCases[i][1], gameCase[1]) == 0){
+                    checkForWin = 1;
+                }
+        }
+        
 
+        if(strcmp(userChoice, pcChoice) == 0){
+            printf("Tie !\n");
+        } else if (checkForWin){
+            printf("You Win!\n");
+        } else {
+            printf("You lose!\n");
+        }
 
-	if(strlen(userChoice) == strlen(pcChoice)){
-		printf("Tie !\n");
-	} else if (checkForWin){
-		printf("You Win!\n");
-	} else {
-		printf("You lose!\n");
-	}
+        printf("\nWould you like to play again? (y/N): \n");
+        
+        scanf("%s", &PlayerInterestedToPlay);
+        PlayerInterestedToPlay = toupper(PlayerInterestedToPlay);
+        
 
+    }
+    while(PlayerInterestedToPlay == 'Y');
+    
 	return 0;
 }
